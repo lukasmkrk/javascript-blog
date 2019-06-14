81cb8b8abb45bc393dd95150e07010de337ed9a8
Lukasz Maciaszek <lukasmkrk@gmail.com>
2019-06-13 21:43:06 -0400

    Add active to the Article

--- js/script.js
+++ js/script.js
@@ -1,23 +1,29 @@
 'use strict';
 
 function titleClickHandler(event){
+  const clickedElement = this;
   console.log('Link was clicked!');
 
-  /* remove class 'active' from all article links  */
+  /* [DONE] remove class 'active' from all article links  */
+
   const activeLinks = document.querySelectorAll('.titles a.active');
 
 for(let activeLink of activeLinks){
   activeLink.classList.remove('active');
 }
 
-  /* add class 'active' to the clicked link */
+  /* [IN PROGRESS] add class 'active' to the clicked link */
+
+clickedElement.classList.add('active');
+console.log('clickedElement:', clickedElement);
+
+  /* [DONE] remove class 'active' from all articles */
+
   const activeArticles = document.querySelectorAll('.post active id.article-1');
 
 for(let activeArticle of activeArticles){
   activeArticle.classList.remove('active');
 }
-
-  /* remove class 'active' from all articles */
 
   /* get 'href' attribute from the clicked link */
 
