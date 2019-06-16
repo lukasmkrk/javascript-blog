9e5dfdd0e4477d47b4b53fb5b8ee000fcab5ba49
Lukasz Maciaszek <lukasmkrk@gmail.com>
2019-06-16 14:21:28 -0400

    Add article Selector

--- js/script.js
+++ js/script.js
@@ -1,31 +1,37 @@
 'use strict';
 
 function titleClickHandler(event){
+  event.preventDefault();
   const clickedElement = this;
   console.log('Link was clicked!');
 
   /* [DONE] remove class 'active' from all article links  */
 
   const activeLinks = document.querySelectorAll('.titles a.active');
-
+debugger;
 for(let activeLink of activeLinks){
   activeLink.classList.remove('active');
 }
 
-  /* [IN PROGRESS] add class 'active' to the clicked link */
+  /* [DONE] add class 'active' to the clicked link */
 
 clickedElement.classList.add('active');
 console.log('clickedElement:', clickedElement);
 
   /* [DONE] remove class 'active' from all articles */
 
-  const activeArticles = document.querySelectorAll('.post active id.article-1');
+  const activeArticles = document.querySelectorAll('.posts .post.active');
 
 for(let activeArticle of activeArticles){
   activeArticle.classList.remove('active');
 }
 
-  /* get 'href' attribute from the clicked link */
+  /* [IN PROGRESS] get 'href' attribute from the clicked link */
+
+  const articleSelector = clickedElement.getAttribute('href');
+  const targetArticle = document.querySelector(articleSelector);
+  targetArticle.classList.add('active');
+  console.log('Link was clicked!')
 
   /* find the correct article using the selector (value of 'href' attribute) */
 
