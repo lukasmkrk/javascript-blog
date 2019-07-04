2550202d865180ccac0acd67eb072ea694a34ff4
Lukasz Maciaszek <lukasmkrk@gmail.com>
2019-07-04 10:26:03 -0400

    Adding new module

--- js/script.js
+++ js/script.js
@@ -191,15 +191,15 @@ const listWrapper = document.querySelector('.list.authors');
 listWrapper.innerHTML = html;
 
 //function generateAuthors() {
-  const articles = document.querySelectorAll(optArticleSelector);
+const articles = document.querySelectorAll(optArticleSelector);
 
-  for (let article of articles) {
-    const authorWrapper = article.querySelector(optArticleAuthorSelector);
-    const articleAuthor = article.getAttribute('data-author');
-    const linkHTML = 'by ' + '<a href="#by' + articleAuthor + '">' + articleAuthor + '</a>';
+for (let article of articles) {
+  const authorWrapper = article.querySelector(optArticleAuthorSelector);
+  const articleAuthor = article.getAttribute('data-author');
+  const linkHTML = 'by ' + '<a href="#by' + articleAuthor + '">' + articleAuthor + '</a>';
 
-    authorWrapper.innerHTML = linkHTML;
-  }
+  authorWrapper.innerHTML = linkHTML;
+}
 }
 
 addClickListenersToAuthors();
@@ -226,3 +226,57 @@ for (let foundAuthorLink of equalAuthorLinks) {
 }
 
 generateTitleLinks(`[data-author="${author}"]`);
+
+// Modul 6.3
+
+function generateTags() {
+  /* [NEW] create a new variable allTags with an empty array */
+  let allTags = [];
+
+  /* find all articles */
+  const articles = document.querySelectorAll(optArticleSelector);
+
+  /* START LOOP: for every article: */
+  for (let article of articles) {
+
+    /* find tags wrapper */
+    const tagsWrapper = article.querySelector(optArticleTagsSelector);
+
+    /* make html variable with empty string */
+    let html = '';
+
+    /* get tags from data-tags attribute */
+    const articleTags = article.getAttribute('data-tags');
+
+    /* split tags into array */
+    const articleTagsArray = articleTags.split(' ');
+
+    /* START LOOP: for each tag */
+    for (let tag of articleTagsArray) {
+
+      /* generate HTML of the link */
+      const linkHTML = '<a href="#tag-' + tag + '">' + tag + '</a>, ';
+
+      /* add generated code to html variable */
+      html = html + linkHTML;
+
+      /* [NEW] check if this link is NOT already in allTags */
+      if (allTags.indexOf(linkHTML) == -1) {
+        /* [NEW] add generated code to allTags array */
+        allTags.push(linkHTML);
+      }
+
+      /* END LOOP: for each tag */
+    }
+
+    /* insert HTML of all the links into the tags wrapper */
+    tagsWrapper.innerHTML = html;
+
+    /* END LOOP: for every article: */
+
+    /* [NEW] find list of tags in right column */
+    const tagList = document.querySelector('.tags');
+
+    /* [NEW] add html from allTags to tagList */
+    tagList.innerHTML = allTags.join(' ');
+  }
